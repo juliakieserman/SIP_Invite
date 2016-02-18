b0a1ef38a534f10aefb0b7c460a4168463950001
Julia Kieserman <jbk67@georgetown.edu>
2016-02-18 12:25:56 -0500

    added a hash function

--- parseFunction.js
+++ parseFunction.js
@@ -1,5 +1,7 @@
 var sip = require('sip');
 var JsSIP = require('jssip');
+var CryptoJS = require("crypto-js");
+var r = require('jsrsasign');
 
 //parse invite message into JSON obj
 //invite message broken into headers
@@ -9,8 +11,7 @@ var m = sip.parse(['INVITE sip:bob@biloxi.com SIP/2.0', 'Via: SIP/2.0/UDP pc33.a
 //does NOT parse correctly -- missing elements
 var invite = sip.parse(['INVITE sip:service@172.16.2.2:5060 SIP/2.0 Via: SIP/2.0/UDP 127.0.1.1:5060;branch=z9hG4bK-1075-1-0 From: sipp <sip:sipp@127.0.1.1:5060>;tag=1075SIPpTag001  To: sut <sip:service@172.16.2.2:5060> Call-ID: 1-1075@127.0.1.1 CSeq: 1 INVITE Contact: sip:sipp@127.0.1.1:5060 Max-Forwards: 70 Subject: Performance Test Content-Type: application/sdp Content-Length:   127 v=0 o=user1 53655765 2353687637 IN IP4 127.0.1.1 s=- c=IN IP4 127.0.1.1 t=0 0 m=audio 6000 RTP/AVP 0 a=rtpmap:0 PCMU/8000', '\r\n'].join('\r\n'));
 
-//extract components for canonical string
-
+//extract components for canonical string:
 //1. AoR of UA sending message or addr-spec of the From header field
 var addrFrom = m["headers"]["from"]["uri"];
 
@@ -34,8 +35,17 @@ if (m.hasOwnProperty("headers"))
 
 //7. message-body
 // DONT KNOW WHAT THIS REFERS TO 
+
 var digestString = addrFrom + "|" + addrTo + "|" + callID;
 
+//hash 
+var hash = CryptoJS.SHA1(digestString);
+
+//sign hash with certificate
+
+
+//console.log(digestString);
+//console.log(hash);
+
 console.log(m);
-console.log(digestString);
 
