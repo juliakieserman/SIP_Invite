93feae27674ba1ba7a412efa7223dffd168dd9d7
Julia Kieserman <jbk67@georgetown.edu>
2016-03-09 08:58:57 -0500

    preliminary PASSporT changes

--- parseFunction.js
+++ parseFunction.js
@@ -7,62 +7,37 @@ var r = require('jsrsasign');
 var _sip = require('sipcore');
 
 
-var privateKey = "-----BEGIN RSA PRIVATE KEY-----\
-   MIICXQIBAAKBgQDPPMBtHVoPkXV+Z6jq1LsgfTELVWpy2BVUffJMPH06LL0cJSQO\
-   aIeVzIojzWtpauB7IylZKlAjB5f429tRuoUiedCwMLKblWAqZt6eHWpCNZJ7lONc\
-   IEwnmh2nAccKk83Lp/VH3tgAS/43DQoX2sndnYh+g8522Pzwg7EGWspzzwIDAQAB\
-   AoGBAK0W3tnEFD7AjVQAnJNXDtx59Aa1Vu2JEXe6oi+OrkFysJjbZJwsLmKtrgtt\
-   PXOU8t2mZpi0wK4hX4tZhntiwGKkUPC3h9Bjp+GerifP341RMyMO+6fPgjqOzUDw\
-   +rPjjMpwD7AkcEcqDgbTrZnWv/QnCSaaF3xkUGfFkLx5OKcRAkEA7UxnsE8XaT30\
-   tP/UUc51gNk2KGKgxQQTHopBcew9yfeCRFhvdL7jpaGatEi5iZwGGQQDVOVHUN1H\
-   0YLpHQjRowJBAN+R2bvA/Nimq464ZgnelEDPqaEAZWaD3kOfhS9+vL7oqES+u5E0\
-   J7kXb7ZkiSVUg9XU/8PxMKx/DAz0dUmOL+UCQH8C9ETUMI2uEbqHbBdVUGNk364C\
-   DFcndSxVh+34KqJdjiYSx6VPPv26X9m7S0OydTkSgs3/4ooPxo8HaMqXm80CQB+r\
-   xbB3UlpOohcBwFK9mTrlMB6Cs9ql66KgwnlL9ukEhHHYozGatdXeoBCyhUsogdSU\
-   6/aSAFcvWEGtj7/vyJECQQCCS1lKgEXoNQPqONalvYhyyMZRXFLdD4gbwRPK1uXK\
-   Ypk3CkfFzOyfjeLcGPxXzq2qzuHzGTDxZ9PAepwX4RSk\
-   -----END RSA PRIVATE KEY-----"
+var m = sip.parse(['INVITE sip:bob@biloxi.com SIP/2.0', 'Via: SIP/2.0/TLS pc33.atlanta.example.com;branch=z9hG4bKnashds8', 'To: Bob <sip:+12155551213@biloxi.com; user=phone>', 'From: Alice <sip:+12155551212@atlanta.com; user=phone>;tag=1928301774', 'Call-ID: a84b4c76e66710', 'CSeq: 314159 INVITE', 'Max-Fowards: 70', 'Date: Thu, 21 Feb 2002 12:02:03 GMT', 'Contact: <sip:alice@pc33.atlanta.example.com>', 'Content-Type: application/sdp', 'Content-Length: 147', '\r\n'].join('\r\n'));
+
+//token header obj
+var header = {"typ":"passport",
+               "algo":"RS256", 
+               "x5u":"https://cert.example.org/passport.crt"};
+
+//get date and convert to milliseconds
+var date = Date.parse(m["headers"]["date"]);
+
+//strip extras from to header
+var to = m["headers"]["to"]["uri"].substring(5, 16);
+
+var from = m["headers"]["from"]["uri"].substring(5, 16);
+
+//token claim obj
+var claims = {"iat": date,
+               "orig": to,
+               "term": from};
 
 
-//make sure SIP library will put it back in teh right order when reconstructing the string 
-//insert new stuff in JSON obj
-var m = sip.parse(['INVITE sip:bob@biloxi.com SIP/2.0', 'Via: SIP/2.0/TLS pc33.atlanta.example.com;branch=z9hG4bKnashds8', 'To: Bob <sip:bob@bioloxi.example.org>', 'From: Alice <sip:alice@atlanta.example.com>;tag=1928301774', 'Call-ID: a84b4c76e66710', 'CSeq: 314159 INVITE', 'Max-Fowards: 70', 'Date: Thu, 21 Feb 2002 12:02:03 GMT', 'Contact: <sip:alice@pc33.atlanta.example.com>', 'Content-Type: application/sdp', 'Content-Length: 147', '\r\n'].join('\r\n'));
-var addrSpecContact = m["headers"]["to"]["uri"];
+//merge two json objects
+var token = {"header":header, 
+             "claims": claims};
 
-/*create the string:
-sip:alice@atlanta.example.com|sip:bob@biloxi.example.org|
-   a84b4c76e66710|314159 INVITE|Thu, 21 Feb 2002 13:02:03 GMT|
-   sip:alice@pc33.atlanta.example.com*/
-var digestString = m["headers"]["from"]["uri"] + "|" + addrSpecContact + "|" + m["headers"]["call-id"] + "|" + m["headers"]["cseq"]["seq"] + ' ' + m["headers"]["cseq"]["method"] + "|" + m["headers"]["date"] + "|" + addrSpecContact;
-
-//does this line reconstruct the sip message?
-var value = sip.stringify(m);
-
-
-//referenced FROM: https://github.com/kjur/jsrsasign/wiki/Tutorial-for-Signature-class
-//initialize
-//ar sig = new KJUR.crypto.Signature({"alg": "SHA1withRSA", "prov": "cryptojs/jsrsa"});
-//initialize for signature generation
-//sip.initSign(privateKey);
-//update data
-//sig.updateString(digestString);
-//calculate signature
-//var sigValueHex = sig.sign();
-
-
-console.log(value);
-
-//hash 
-//var hash = CryptoJS.SHA256(digestString);
-
-//sign hash with certificate
-
-
-//console.log(digestString);
 
 
 //convert back to string
+var value = sip.stringify(m);
 
-
+console.log(token);
 //console.log(m);
+
 
